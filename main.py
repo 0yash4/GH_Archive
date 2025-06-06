8c69161792740f117a9e8c8af67e61bc2a0d268a
0yash4 <4yashchoudhary@gmail.com>
2025-06-06 13:34:09 -0500

    setting up wsl uv python env

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import sys
 
 def main():
     print(sys.executable)
-    print(sys.version)
-    print(sys.platform)
-    print(sys.path)
-    print(sys.argv)
-    print(sys.modules)
-    print(sys.getdefaultencoding())
 
 
 if __name__ == "__main__":
